13a74a5272b5abee3f8b6b24d36251ba2817df74
Mitch Webster <mitchell.a.webster@gmail.com>
2024-02-18 07:57:17 -0800

    Update agent.py

--- dist/agent.py
+++ dist/agent.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import agent_pb2 # Required do not edit
+import random
+
+def PerformFantasyActions(landscape):
+    print(landscape)
+    print("Performing bot actions")
+
+    flip = random.randint(0, 1)
+    bet = flip == 0
+
+    return agent_pb2.FantasySelections(
+        make_bet=bet
+    )
